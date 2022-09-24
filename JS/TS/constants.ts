b53ec106c6262c9853ffb4a1dcdf6af2a09148fb
Nikita Ivanov <iwaa0303@mail.ru>
2022-09-24 20:33:05 +0300

    removed useless

--- JS/TS/constants.ts
+++ JS/TS/constants.ts
@@ -4,7 +4,4 @@ export class constants {
     static FIELD_Y: object = $('#y_value');
     static FIELD_X: object = $('#x_value');
     static FIELD_R: object = $('#r_value');
-    static TABLE: object = $('#table_id');
-    static dataset: string[] = [];
-    static srt: string | number | string[] = $(constants.TABLE).val();
 }
